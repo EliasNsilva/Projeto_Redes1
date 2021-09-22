54f3d32b1e092d869fc008a3c6964b90495ee59c
EliasNsilva <57777232+EliasNsilva@users.noreply.github.com>
2021-09-22 13:42:42 -0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -18,10 +18,13 @@ def send_messages(issuer, message): #função para enviar as mensagem a todos do
 
 def process(server_input,address):
     while True: 
-        response = server_input.recv(1024).decode('UTF-8') #recebe os codigo do das opção escolhidas pelo cliente
+        response = server_input.recv(1024).decode('UTF-8')
         if (response == "CC"):
             login = server_input.recv(1024).decode('UTF-8')
+            server_input.send("L_OK".encode('UTF-8'))
             senha = server_input.recv(1024).decode('UTF-8')
+            server_input.send("S_OK".encode('UTF-8'))
+
             dados[login] = senha
             database.atualizar(dados) #adiciona o usuario da base de dados
 
@@ -63,9 +66,7 @@ def process(server_input,address):
                 server_input.send("!LOG_ERR".encode('UTF-8')) #envia um codigo de erro ao cliente
             
         elif(response == "DSC"):
-            send_messages(server_input, "Saiu".encode('UTF-8'))
             print ("Conexão de {} encerrada ".format(address))
-            server_input.close()
 
 
 while True:
